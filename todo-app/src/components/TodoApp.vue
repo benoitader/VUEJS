c071a316fefe11c111fa6789aa2ec8f38ec7271e
benoitader <benoitader.dev@gmail.com>
2022-04-14 16:36:19 +0200

    Initialisation de TodoApp.vue

---
+++ todo-app/src/components/TodoApp.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">Ma Todo List Application</h2>
+
+    <div class="d-flex">
+      <input type="text" placeholder="Entrer une tâche" class="form-control">
+      <button @click="submitTache" class="btn btn-warning rounded-0">Entrer</button>
+    </div>
+
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Tâches</th>
+          <th scope="col">Statut</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(tache, index) in taches" :key="index">
+          <th>{{tache.name}}</th>
+          <td>{{tache.statut}}</td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  data(){
+    return {
+      taches: [
+        {
+          name: 'Dire à Hugo de fermer sa gueule.',
+          statut: 'Fait'
+        },
+        {
+          name: 'Manger le riz curry de Benoit.',
+          statut: 'En cours'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    submitTache(){
+      console.log('Bonjour Benoit.')
+    }
+  }
+};
+
+</script>
+
+<style scoped>
+</style>
