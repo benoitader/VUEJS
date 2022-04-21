4107d5d1f19655c8c20c602fdabaa8d1f9c740c2
benoitader <benoitader.dev@gmail.com>
2022-04-21 09:51:45 +0200

    Création de la View 'Bières'

--- projet-m1-vuejs/src/router/index.js
+++ projet-m1-vuejs/src/router/index.js
@@ -7,16 +7,16 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'accueil',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/bieres',
+    name: 'bieres',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/BieresView.vue')
   }
 ]
 
---
+++ projet-m1-vuejs/src/views/BieresView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>Ma page de bières de ces morts</h1>
+  </div>
+</template>
+
+<script>
+  
+</script>
